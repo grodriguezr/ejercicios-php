ca136cb4e7708758a4e74a7c43bad571062b21e3
DANIEL RODRIGUEZ UDC <drodriguezt1@unicartagena.edu.co>
2025-03-01 19:17:54 -0500

    se agraga, set and get, de apellido

--- ejercicio1/index.php
+++ ejercicio1/index.php
@@ -1,11 +1,12 @@
 <?php
     include('persona.php');
     $p = new Persona(); //instancia de clase persona
-    $p->set_nombre('Pedro'); //asigné valor al atributo nombre
-    
+    $p->set_nombre('Daniel'); //asigné valor al atributo nombre
     $p->imprimeInformacion(); //llamo a la función imprimeInformación
+    $p->set_nombre('pedro');
+    $p->imprimeInformacion();
 
-
-
-
+    $p2 = new Persona();
+    $p2->set_nombre('eduardo');
+    $p2->imprimeInformacion(); 
 ?>
